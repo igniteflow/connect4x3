579b8b6efbaca57d06e41941a8cd088bf7c6dbb8
sthorne <sthorne@osdc.com.au>
2011-09-09 05:21:56 +1000

    Improved printing of the game state.

--- connect4.py
+++ connect4.py
@@ -1,20 +1,31 @@
 
 
+ROWS = 6
+COLUMNS = 7
+
 def print_board(board):
-    for row in board:
+    for row in zip(*board):
         for column in row:
-            print column,
+            print (column if column else '.'),
         print
+
+    for column in range(7):
+        print column+1,
+    print
 
 def win_state(board):
     return False
 
 def main():
-    board = [[None for x in range(6)] for y in range(7)]
+    board = [[None for x in range(ROWS)] for y in range(COLUMNS)]
+
+    current_player = 'X'
+    other_player = 'O'
     
     while True:
-        data = raw_input()
         print_board(board)
+
+        data = raw_input()
         if win_state(board):
             print 'Winner!'
             return
